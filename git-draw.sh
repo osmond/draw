cebc42f92d1bb48c7277a287d176f2dd99195642
Jon Osmond <j@osmond.cc>
2016-01-20 14:43:28 -0600

    Draw1

---
+++ git-draw.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+REPO=gf
+git init $REPO
+cd $REPO
+echo "Created with Git Draw (http://github.com/ben174/git-draw)" > README.md
+git add README.md
+touch gf
+git add gf
+echo 0 >> gf
+GIT_AUTHOR_DATE=2015-04-13T12:00:00 GIT_COMMITTER_DATE=2015-04-13T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-13T12:00:00 GIT_COMMITTER_DATE=2015-04-13T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-04-13T12:00:00 GIT_COMMITTER_DATE=2015-04-13T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-13T12:00:00 GIT_COMMITTER_DATE=2015-04-13T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-04-14T12:00:00 GIT_COMMITTER_DATE=2015-04-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-04-14T12:00:00 GIT_COMMITTER_DATE=2015-04-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-04-14T12:00:00 GIT_COMMITTER_DATE=2015-04-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-04-14T12:00:00 GIT_COMMITTER_DATE=2015-04-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-04-15T12:00:00 GIT_COMMITTER_DATE=2015-04-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-15T12:00:00 GIT_COMMITTER_DATE=2015-04-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-04-15T12:00:00 GIT_COMMITTER_DATE=2015-04-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-15T12:00:00 GIT_COMMITTER_DATE=2015-04-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-04-16T12:00:00 GIT_COMMITTER_DATE=2015-04-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-04-16T12:00:00 GIT_COMMITTER_DATE=2015-04-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-04-16T12:00:00 GIT_COMMITTER_DATE=2015-04-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-04-16T12:00:00 GIT_COMMITTER_DATE=2015-04-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-04-17T12:00:00 GIT_COMMITTER_DATE=2015-04-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-17T12:00:00 GIT_COMMITTER_DATE=2015-04-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-04-17T12:00:00 GIT_COMMITTER_DATE=2015-04-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-17T12:00:00 GIT_COMMITTER_DATE=2015-04-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-04-22T12:00:00 GIT_COMMITTER_DATE=2015-04-22T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-04-22T12:00:00 GIT_COMMITTER_DATE=2015-04-22T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-04-22T12:00:00 GIT_COMMITTER_DATE=2015-04-22T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-04-22T12:00:00 GIT_COMMITTER_DATE=2015-04-22T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-04-29T12:00:00 GIT_COMMITTER_DATE=2015-04-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-29T12:00:00 GIT_COMMITTER_DATE=2015-04-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-04-29T12:00:00 GIT_COMMITTER_DATE=2015-04-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-04-29T12:00:00 GIT_COMMITTER_DATE=2015-04-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-04T12:00:00 GIT_COMMITTER_DATE=2015-05-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-04T12:00:00 GIT_COMMITTER_DATE=2015-05-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-04T12:00:00 GIT_COMMITTER_DATE=2015-05-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-04T12:00:00 GIT_COMMITTER_DATE=2015-05-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-05T12:00:00 GIT_COMMITTER_DATE=2015-05-05T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-05T12:00:00 GIT_COMMITTER_DATE=2015-05-05T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-05T12:00:00 GIT_COMMITTER_DATE=2015-05-05T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-05T12:00:00 GIT_COMMITTER_DATE=2015-05-05T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-06T12:00:00 GIT_COMMITTER_DATE=2015-05-06T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-06T12:00:00 GIT_COMMITTER_DATE=2015-05-06T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-06T12:00:00 GIT_COMMITTER_DATE=2015-05-06T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-06T12:00:00 GIT_COMMITTER_DATE=2015-05-06T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-07T12:00:00 GIT_COMMITTER_DATE=2015-05-07T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-07T12:00:00 GIT_COMMITTER_DATE=2015-05-07T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-07T12:00:00 GIT_COMMITTER_DATE=2015-05-07T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-07T12:00:00 GIT_COMMITTER_DATE=2015-05-07T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-08T12:00:00 GIT_COMMITTER_DATE=2015-05-08T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-08T12:00:00 GIT_COMMITTER_DATE=2015-05-08T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-08T12:00:00 GIT_COMMITTER_DATE=2015-05-08T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-08T12:00:00 GIT_COMMITTER_DATE=2015-05-08T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-25T12:00:00 GIT_COMMITTER_DATE=2015-05-25T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-25T12:00:00 GIT_COMMITTER_DATE=2015-05-25T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-25T12:00:00 GIT_COMMITTER_DATE=2015-05-25T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-25T12:00:00 GIT_COMMITTER_DATE=2015-05-25T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-26T12:00:00 GIT_COMMITTER_DATE=2015-05-26T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-26T12:00:00 GIT_COMMITTER_DATE=2015-05-26T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-26T12:00:00 GIT_COMMITTER_DATE=2015-05-26T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-26T12:00:00 GIT_COMMITTER_DATE=2015-05-26T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-27T12:00:00 GIT_COMMITTER_DATE=2015-05-27T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-27T12:00:00 GIT_COMMITTER_DATE=2015-05-27T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-27T12:00:00 GIT_COMMITTER_DATE=2015-05-27T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-27T12:00:00 GIT_COMMITTER_DATE=2015-05-27T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-28T12:00:00 GIT_COMMITTER_DATE=2015-05-28T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-28T12:00:00 GIT_COMMITTER_DATE=2015-05-28T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-28T12:00:00 GIT_COMMITTER_DATE=2015-05-28T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 >> gf
+GIT_AUTHOR_DATE=2015-05-28T12:00:00 GIT_COMMITTER_DATE=2015-05-28T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 >> gf
+GIT_AUTHOR_DATE=2015-05-29T12:00:00 GIT_COMMITTER_DATE=2015-05-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-29T12:00:00 GIT_COMMITTER_DATE=2015-05-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
+GIT_AUTHOR_DATE=2015-05-29T12:00:00 GIT_COMMITTER_DATE=2015-05-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 >> gf
+GIT_AUTHOR_DATE=2015-05-29T12:00:00 GIT_COMMITTER_DATE=2015-05-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 >> gf
